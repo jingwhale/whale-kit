b2840fb9b06c4f9fbbad7fc65932f597936f54f4
whaleExplorer <whaleExplorer@gmail.com>
2019-03-21 21:51:12 +0800

    @add create button code

--- src/generateButtonCommand.js
+++ src/generateButtonCommand.js
@@ -1,16 +1,11 @@
 import sketch from 'sketch'
-import BrowserWindow from 'sketch-module-web-view'
 import Sketch from 'sketch/dom'
-
+var  Group = Sketch.Group;
+var Text = Sketch.Text;
 var Document = Sketch.Document;
 var document = Document.getSelectedDocument();
 
 var page = document.selectedPage;
-console.log(page)
-
-if(!page){
-    return;
-}
 
 var selection = document.selectedLayers;
 
@@ -18,12 +13,31 @@ var buttonRect = "";
 
 selection.forEach(layer => (buttonRect=layer))
 
+export default function(context) {
+    if(!buttonRect){//选择一个矩形
+        sketch.UI.message("请选择一个矩形！")
+    }else{
+        //Create Body(Group)
+        const group = new Group({
+            name: 'button group',
+            parent:(buttonRect.parent || page)
+        })
 
-if(!buttonRect){
-   export default function() {
-       sketch.UI.message("请选择一个矩形！")
-   }
-    return;
-}else{
+        buttonRect.parent = group;
 
+        //Create Body(Group)-Text
+        const text = new Text({
+            text: 'button',
+            alignment: Text.Alignment.center,
+            frame: buttonRect.frame,
+            parent: group,
+            name: "text"
+        })
+
+        text.style.verticalAlignment = "center";
+        text.style.fontSize = (buttonRect.frame.height*0.4);
+        text.style.lineHeight = buttonRect.frame.height;
+        text.style.width = buttonRect.frame.width;
+        text.style.height = buttonRect.frame.height;
+    }
 }
